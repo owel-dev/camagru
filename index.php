97eeaa390a86f65110f74238f4499d5b6dc52d7e
owel-dev <mail@owel.dev>
2023-04-13 15:49:22 +0000

    feat: index.php 추가 및 메인 페이지 기본 그리드 설정

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello Camagru</title>
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+
+      </div>
+      <div class="user">
+
+      </div>
+    </header>
+    <main>
+      <div class="contents">
+        
+      </div>
+    </main>
+  </body>
+</html>
+
+
