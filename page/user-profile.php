27fa4b1aa70cfa9437cb537c8d2c1ef0a317922e
owel-dev <mail@owel.dev>
2023-04-21 09:10:52 +0000

    refactor: user-profile페이지 파일 분리
    - profile 파일의 이름을 user-profile로 변경

---
+++ page/user-profile.php
@@ -0,0 +1,36 @@
+<?php include '../config.php';?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>profile</title>
+        <link rel="stylesheet" href="<?php echo CSS_PATH;?>reset.css" />
+        <link rel="stylesheet" href="<?php echo CSS_PATH;?>modal-signup.css" />
+        <link rel="stylesheet" href="<?php echo CSS_PATH;?>header.css" />
+        <link rel="stylesheet" href="<?php echo CSS_PATH;?>user-profile.css" />
+    </head>
+    <body>
+        <header>
+            <?php include COMPONENT_PATH . 'header.php'; ?>
+        </header>
+        <main>
+            <?php include COMPONENT_PATH . 'modal-signup.php';  ?>
+            <div class="contents">
+                <div class="profile-form">
+                    <form action="">
+                        <label>User Name</label>
+                        <input type="text">
+
+                        <label>Email</label>
+                        <input type="text">
+
+                        <label>Password</label>
+                        <input type="text">
+
+                        <button type="submit">제출</button>
+                    </form>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
