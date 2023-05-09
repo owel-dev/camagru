dea82286d2d06901490e8311492598fc63052d53
owel-dev <mail@owel.dev>
2023-05-09 10:50:37 +0000

    refactor: modal.js 필요없는 라인 삭제

--- public/js/modal.js
+++ public/js/modal.js
@@ -2,22 +2,16 @@ function handleModal(event) {
     var modal = document.querySelector('.modal');
     var modal_div = document.querySelector('.modal-div');
 
-    if (modal.style.display === 'none'){
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                modal_div.innerHTML = xhr.responseText;
-                modal.style.display = 'flex';
-            }
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            modal.style.display = 'flex';
+            modal_div.innerHTML = xhr.responseText;
         }
-        
-        var url = `/view/include/${event.target.id}.php`;
-        xhr.open('GET', url, true);
-        xhr.send();
-    } else {
-        modal_div.innerHTML = '';
-        modal.style.display = 'none';
     }
+    var url = `/view/include/${event.target.id}.php`;
+    xhr.open('GET', url, true);
+    xhr.send(); 
 };
 
 document.addEventListener('DOMContentLoaded', function() {
